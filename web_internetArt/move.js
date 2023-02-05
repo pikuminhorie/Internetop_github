f9efb85e31aa9930b063ae62f36e31f20e81853d
kumi <newkumi.ho@gmail.com>
2023-02-05 15:47:37 +0900

    js

--- web_internetArt/move.js
+++ web_internetArt/move.js
@@ -16,3 +16,16 @@ function setup() {
     ellipse(mouseX, mouseY, 100);
   }
   
+
+// 前表示のためのjsなので以下ノータッチ
+  function init() {
+    var wpr = document.getElementById("wrapper");
+    var cvs = document.getElementById("everyone");
+ 
+    cvs.width = wpr.offsetWidth;
+    cvs.height = wpr.offsetHeight;
+ 
+    window.alert("screen.width " + screen.width + "、" + "screen.height " + screen.height + "\n"
+                 + "cvs.width " + cvs.width + "、" + "cvs.height " + cvs.height);
+  }
+ 
